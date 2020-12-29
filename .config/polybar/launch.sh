ee847dc707667b3aa885b884c8f86cc71eeab47d
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2020-12-29 12:43:50 +0200

    polybar workspaces, temp, batt; picom transitions between workspaces; set up .gitignore and added all files

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # polybar -rq dummy & 
-polybar -rq music &
+
+# polybar -rq music &
 polybar -rq tray &
 polybar -rq i3 &
 
