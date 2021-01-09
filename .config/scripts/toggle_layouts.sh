20615c1af18d3eb2adf3286aae2a2a09230cf550
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2021-01-09 19:52:56 +0200

    Shortcut and script for toggling and reloading keyboard layouts; some polybar changes I guess

---
+++ .config/scripts/toggle_layouts.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.9
+
+def read_and_change(path:str):
+    lines = []
+    with open(path, 'r') as config:
+        for line in config:
+            
+            if '-layout' in line and 'grp:ctrl_alt_toggle' in line:
+                print(line, end='')
+                if line[0] == '#':
+                    line = line[1:]
+                    print('****', line, end='')
+                else:
+                    line = '#'+line
+                    print('####', line, end='')
+                print(line, end='')
+            lines.append(line)
+    return lines
+
+def write_new_config(path:str, lines:list):
+    with open(path, 'w') as new_config:
+        for line in lines:
+            #print(line, end='')
+            new_config.write(line)
+
+def toggle_layouts(path):
+    alt_lines = read_and_change(path)
+    write_new_config(path, alt_lines)
+
+
+toggle_layouts('/home/archy/.config/i3/config')
