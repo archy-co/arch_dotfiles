68b101af9487d9c57671ad1d798dd43775d18bf8
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2021-08-18 14:17:24 +0300

    Move trays into polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,6 +9,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # polybar -rq dummy & 
 
 # polybar -rq music &
+polybar -rq notifications &
+sleep 1
 polybar -rq tray &
 polybar -rq options &
 polybar -rq i3 &
