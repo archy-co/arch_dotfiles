96339b4dfbda2df12ee9eca0dfc8a31d5099febb
Archy <archy.revera@protonmail.com>
2021-12-29 23:31:34 +0200

    Sufficient configurations of flycheck, irony, YAS, eldoc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,15 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes '(deeper-blue))
- '(dired-listing-switches "-l")
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes '(wombat))
  '(display-line-numbers t)
- '(irony-lighter " Irony Running")
+ '(electric-indent-mode t)
+ '(global-flycheck-mode t)
  '(package-selected-packages
-   '(eldoc zweilight-theme company-irony flycheck-irony company irony-eldoc irony))
- '(x-gtk-show-hidden-files nil))
+   '(yasnippet-snippets smartparens yasnippet company-irony flycheck-irony irony-eldoc irony)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,7 +18,9 @@
  ;; If there is more than one, they won't work right.
  )
 
- 
+
+
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
@@ -27,24 +28,20 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-					; Packages installed:
-					; irony
-					; flycheck-irony
-                    ; company-irony
-                    ; irony-eldoc
 
-; Irony-mode clang lsp for c and c++
+
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
+
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 
-; Syntax checking
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+(add-hook 'irony-mode-hook #'irony-eldoc)
+(add-hook 'irony-mode-hook #'smartparens-mode)
 
+(add-hook 'after-init-hook 'global-company-mode)
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-irony))
 
-(add-hook 'irony-mode-hook #'irony-eldoc)
 
+(add-hook 'irony-mode-hook #'yas-minor-mode)
