5a2a1bbcee79572bdaae2b817dfdc6d01eb8b0ca
Archy <archy.revera@protonmail.com>
2022-02-06 23:59:19 +0200

    Update emacs configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,6 @@
+;;; init.el --- Emacs configurations
+;;; Commentary:
+;;; Code:
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,14 +13,17 @@
  '(electric-indent-mode t)
  '(global-flycheck-mode t)
  '(package-selected-packages
-   '(yasnippet-snippets smartparens yasnippet company-irony flycheck-irony irony-eldoc irony)))
+   '(cmake-project git-gutter diff-hl markdown-mode mmm-mode flymd yasnippet-snippets smartparens yasnippet company-irony flycheck-irony irony-eldoc irony))
+ '(save-place-mode t)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(size-indication-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
+ '(default ((t (:family "SF Mono" :foundry "APPL" :slant normal :weight normal :height 98 :width normal)))))
 
 
 
@@ -40,8 +46,36 @@
 (add-hook 'irony-mode-hook #'smartparens-mode)
 
 (add-hook 'after-init-hook 'global-company-mode)
+
+(defvar company-backends)
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-irony))
 
-
+(add-hook 'python-mode #'yas-minor-mode)
 (add-hook 'irony-mode-hook #'yas-minor-mode)
+
+(autoload 'cmake-project-mode "cmake-project" nil t)
+(defun maybe-cmake-project-mode ()
+  (if (or (file-exists-p "CMakeLists.txt")
+          (file-exists-p (expand-file-name "CMakeLists.txt" (car (project-roots (project-current))))))
+      (cmake-project-mode)))
+
+(add-hook 'c-mode-hook 'maybe-cmake-project-mode)
+(add-hook 'c++-mode-hook 'maybe-cmake-project-mode)
+
+
+(global-git-gutter-mode +1)
+
+(require 'mmm-mode)
+(setq mmm-global-mode 'maybe)
+
+(mmm-add-classes
+ '((markdown-lisp
+    :submode lisp-mode
+    :front "^```lisp[\n\r]+"
+    :back "^```$")))
+
+(mmm-add-mode-ext-class 'markdown-mode nil 'markdown-lisp)
+
+(provide 'init)
+;;; init.el ends here
